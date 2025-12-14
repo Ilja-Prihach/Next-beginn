aadb7423c82f49fa67194069b8e0f0b3ec18e876
Ilja-Prihach <piratehnik9508@gmail.com>
2025-12-14 15:02:11 +0300

    use in  project useRouter()

--- src/widgets/header.tsx
+++ src/widgets/header.tsx
@@ -1,15 +1,22 @@
 "use client"
 
 import styles from "./header.module.css"
-import Link from "next/link";
+import {useRouter} from "next/navigation";
+
 
 export const Header = () => {
+
+    const router = useRouter();
+
+
     return (
         <nav className={styles.navigation}>
             <ul>
-                <li><Link href="/hello" className={styles.link}>Main</Link></li>
-                <li><Link href="/profile" className={styles.link}> Profile</Link></li>
-                <li><Link href="/profile/123" className={styles.link}>Profile 123 </Link></li>
+                <li><button  onClick={() => router.back()} className={styles.link}>Back</button></li>
+                <li><button  onClick={() => router.push("/hello")} className={styles.link}>Main</button></li>
+                <li><button  onClick={() => router.push("/profile", {scroll: false})} className={styles.link}> Profile</button></li>
+                <li><button  onClick={() => router.push("/profile/123")} className={styles.link}>Profile 123 </button></li>
+                <li><button  onClick={() => router.forward()} className={styles.link}>Forward</button></li>
             </ul>
         </nav>
     )
